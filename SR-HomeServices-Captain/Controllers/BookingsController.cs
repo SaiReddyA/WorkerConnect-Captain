1a161101ceb236099e36d93b19eb843fd04e5bb3
Sai Reddy Local <saireddy1m43@gmail.com>
2026-01-12 21:31:23 +0530

    controller change

---
+++ SR-HomeServices-Captain/Controllers/BookingsController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SR_HomeServices_Captain.Controllers
+{
+    public class BookingsController : Controller
+    {
+        // GET: BookingsController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: BookingsController
+        public ActionResult GetPendingRequests()
+        {
+            return PartialView();
+        }
+
+        // GET: BookingsController
+        public ActionResult GetInProgressRequests()
+        {
+            return PartialView();
+        }
+
+        // GET: BookingsController
+        public ActionResult GetCompletedRequests()
+        {
+            return PartialView();
+        }
+
+        // GET: BookingsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: BookingsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BookingsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BookingsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: BookingsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BookingsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: BookingsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
